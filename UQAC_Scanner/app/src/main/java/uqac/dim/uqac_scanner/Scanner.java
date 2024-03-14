af6293b2be8244993b9e17217dfb0a1181542dfe
dev-2031887 <102762254+Froggy1298@users.noreply.github.com>
2024-03-13 21:59:58 -0400

    Added the navigation between page

---
+++ UQAC_Scanner/app/src/main/java/uqac/dim/uqac_scanner/Scanner.java
@@ -0,0 +1,21 @@
+package uqac.dim.uqac_scanner;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Scanner extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        Log.i("LOG", "Scanner onCreateView");
+        return inflater.inflate(R.layout.fragment_scanner, container, false);
+    }
+}
