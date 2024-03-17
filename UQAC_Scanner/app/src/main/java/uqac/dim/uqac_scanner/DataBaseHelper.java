b1c5f800651cd507080826b0267bd5dd9a3ddbf2
Laurie-Anne L <102762236+LaurieAnneuh@users.noreply.github.com>
2024-03-17 12:42:02 -0400

    Creating the data base helper
    - Model QR Code
    - Create database
    - Add to database
    - Get all qr in database

---
+++ UQAC_Scanner/app/src/main/java/uqac/dim/uqac_scanner/DataBaseHelper.java
@@ -0,0 +1,78 @@
+package uqac.dim.uqac_scanner;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class DataBaseHelper extends SQLiteOpenHelper {
+    public static final String QR_TABLE = "QR_TABLE";
+    public static final String COLUMN_QR_NAME = "QR_NAME";
+    public static final String COLUMN_QR_URL = "QR_URL";
+    public static final String COLUMN_QR_DESCRIPTION = "QR_DESCRIPTION";
+    public static final String COLUMN_QR_DATE_CREATE = "QR_DATE_CREATE";
+    public static final String COLUMN_QR_DATE_EDIT = "QR_DATE_EDIT";
+    public static final String COLUMN_QR_IS_SCANNED = "QR_IS_SCANNED";
+    public DataBaseHelper(@Nullable Context context) {
+        super(context, "qr.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTableStatement = "CREATE TABLE " + QR_TABLE + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_QR_NAME + " TEXT, " + COLUMN_QR_URL + " TEXT, " +
+                COLUMN_QR_DESCRIPTION + " TEXT, " + COLUMN_QR_DATE_CREATE + " DATE, " + COLUMN_QR_DATE_EDIT + " DATE, " + COLUMN_QR_IS_SCANNED +" BOOL)";
+        db.execSQL(createTableStatement);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public boolean addQR(QrCodeModel qrCodeModel) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(COLUMN_QR_NAME, qrCodeModel.getName());
+        cv.put(COLUMN_QR_URL, qrCodeModel.getUrl());
+        cv.put(COLUMN_QR_DESCRIPTION, qrCodeModel.getDescription());
+        cv.put(COLUMN_QR_DATE_CREATE, qrCodeModel.getDateCreation().getTime());
+        cv.put(COLUMN_QR_DATE_EDIT, qrCodeModel.getDateEdit().getTime());
+        cv.put(COLUMN_QR_IS_SCANNED, qrCodeModel.isScanned());
+
+        long insert = db.insert(QR_TABLE, null, cv);
+
+        return insert != -1;
+    }
+
+    public List<QrCodeModel> getAllQR() {
+        List<QrCodeModel> returnList = new ArrayList<>();
+        String query = "SELECT * FROM " + QR_TABLE;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                int qrID = cursor.getInt(0);
+                String qrName = cursor.getString(1);
+                String qrUrl = cursor.getString(2);
+                String qrDesc = cursor.getString(3);
+                Date qrDateCreate = new Date(cursor.getLong(4)*1000);
+                Date qrDateEdit = new Date(cursor.getLong(4)*1000);
+                boolean qrIsScanned = cursor.getInt(6) == 1;
+
+                returnList.add(new QrCodeModel(qrID,qrName,qrUrl,qrDesc,qrDateCreate,qrDateEdit,qrIsScanned));
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return returnList;
+    }
+}
---
+++ UQAC_Scanner/app/src/main/java/uqac/dim/uqac_scanner/QrCodeModel.java
@@ -0,0 +1,87 @@
+package uqac.dim.uqac_scanner;
+
+import java.util.Date;
+
+public class QrCodeModel {
+    private Integer ID;
+    private String name;
+    private String url;
+    private String description;
+    //private  codeQR;
+    private Date dateCreation;
+    private Date dateEdit;
+    private boolean isScanned;
+    public QrCodeModel(){}
+    public QrCodeModel(String name, String url, String description, Date dateCreation, Date dateEdit, boolean isScanned){
+        this.name = name;
+        this.url = url;
+        this.description = description;
+        this.dateCreation = dateCreation;
+        this.dateEdit = dateEdit;
+        this.isScanned = isScanned;
+    }
+    public QrCodeModel(Integer id, String name, String url, String description, Date dateCreation, Date dateEdit, boolean isScanned) {
+        this.ID = id;
+        this.name = name;
+        this.url = url;
+        this.description = description;
+        this.dateCreation = dateCreation;
+        this.dateEdit = dateEdit;
+        this.isScanned = isScanned;
+    }
+    //getter and setter
+    public String getName() {
+        return name;
+    }
+    public void setName(String value) {
+        name = value;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDateCreation() {
+        return dateCreation;
+    }
+
+    public void setDateCreation(Date dateCreation) {
+        this.dateCreation = dateCreation;
+    }
+
+    public Date getDateEdit() {
+        return dateEdit;
+    }
+
+    public void setDateEdit(Date dateEdit) {
+        this.dateEdit = dateEdit;
+    }
+
+    public boolean isScanned() {
+        return isScanned;
+    }
+
+    public void setScanned(boolean scanned) {
+        isScanned = scanned;
+    }
+
+    public Integer getID() {
+        return ID;
+    }
+
+    public void setID(Integer ID) {
+        this.ID = ID;
+    }
+}
