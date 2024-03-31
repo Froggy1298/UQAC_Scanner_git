020ada157750027a28d544907fdc9186b6c72bbe
dev-2031887 <102762254+Froggy1298@users.noreply.github.com>
2024-03-31 01:55:18 -0400

    Update MainActivity.java

--- UQAC_Scanner/app/src/main/java/uqac/dim/uqac_scanner/MainActivity.java
+++ UQAC_Scanner/app/src/main/java/uqac/dim/uqac_scanner/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.i("LOG", "Weee");
+        Log.i("LOG", "Weee");
         dataBaseHelper = new DataBaseHelper(MainActivity.this);
 
 
