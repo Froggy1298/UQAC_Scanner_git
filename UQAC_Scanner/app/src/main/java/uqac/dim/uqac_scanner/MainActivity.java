63940ee75431adaa4ed13723a5d712e33ea9c588
dev-2031887 <102762254+Froggy1298@users.noreply.github.com>
2024-03-31 02:31:33 -0400

    delete test

--- UQAC_Scanner/app/src/main/java/uqac/dim/uqac_scanner/MainActivity.java
+++ UQAC_Scanner/app/src/main/java/uqac/dim/uqac_scanner/MainActivity.java
@@ -32,10 +32,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Log.i("LOG", "Weee");
-        Log.i("LOG", "Weee");
-        Log.i("LOG", "Weee");
+        
         dataBaseHelper = new DataBaseHelper(MainActivity.this);
 
 
