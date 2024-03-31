11efa8ab078f9ac5242e88c884f3f82b39a7ae0c
dev-2031887 <102762254+Froggy1298@users.noreply.github.com>
2024-03-31 01:59:04 -0400

    Update MainActivity.java

--- UQAC_Scanner/app/src/main/java/uqac/dim/uqac_scanner/MainActivity.java
+++ UQAC_Scanner/app/src/main/java/uqac/dim/uqac_scanner/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
 
         Log.i("LOG", "Weee");
         Log.i("LOG", "Weee");
+        Log.i("LOG", "Weee");
         dataBaseHelper = new DataBaseHelper(MainActivity.this);
 
 
