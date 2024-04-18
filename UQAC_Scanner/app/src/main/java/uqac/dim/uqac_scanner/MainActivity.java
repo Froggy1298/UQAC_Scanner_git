98cc84fd818debae0284fc94e79a0c12b0f75441
biiintx <129656360+biiintx@users.noreply.github.com>
2024-04-18 01:15:27 -0400

    Update MainActivity.java

--- UQAC_Scanner/app/src/main/java/uqac/dim/uqac_scanner/MainActivity.java
+++ UQAC_Scanner/app/src/main/java/uqac/dim/uqac_scanner/MainActivity.java
@@ -1,14 +1,8 @@
 package uqac.dim.uqac_scanner;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,60 +15,33 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import uqac.dim.uqac_scanner.CreateFolder.Create;
 import uqac.dim.uqac_scanner.HistoryFolder.History;
-import uqac.dim.uqac_scanner.R;
 import uqac.dim.uqac_scanner.ScannerFolder.Scanner;
 import uqac.dim.uqac_scanner.Helpers.DataBaseHelper;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final int REQUEST_PICK_IMAGE = 1;
 
     private DataBaseHelper dataBaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_scanner);
+        setContentView(R.layout.activity_main);
 
         // Initialisation des vues et des écouteurs d'événements
         initViews();
 
         // Initialisation de la base de données
         dataBaseHelper = new DataBaseHelper(MainActivity.this);
-
-        // Chargement du fragment Scanner dès que l'activité est créée
-        loadScannerFragment();
     }
 
-    // Initialisation des vues et des écouteurs d'événements
     private void initViews() {
-        //TODO revert to the old way
-
-        //TODO ça plante là
-        // Gestion de la navigation inférieure
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_Navigation_View);
         bottomNavigationView.setOnItemSelectedListener(this::onNavigationItemSelected);
 
-        // Gestion du bouton flottant
         FloatingActionButton createButton = findViewById(R.id.but_Create);
         createButton.setOnClickListener(this::onClickAction);
-
-        // Gestion du bouton "De ma bibliothèque"
-        Button fromLibraryButton = findViewById(R.id.from_library_button);
-        fromLibraryButton.setOnClickListener(this::onFromLibraryButtonClick);
     }
 
-    //TODO mettre cela dans le "Scanner" class
-    // Méthode pour charger le fragment Scanner
-    private void loadScannerFragment() {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.frame_layout, new Scanner());
-        fragmentTransaction.commit();
-    }
-
-
-    //TODO Ne pas changer cette fonction, il va avoir beaucoup de conflit avec main si tu changes cette fonction
     // Gestionnaire d'événements pour la navigation inférieure
     public boolean onNavigationItemSelected(MenuItem item) {
         try {
@@ -96,7 +63,6 @@ public class MainActivity extends AppCompatActivity {
         loadFragment(new Create(), false);
     }
 
-    //TODO Revert to the old way
     // Méthode pour charger un fragment dans le conteneur
     private boolean loadFragment(Fragment fragment, boolean initializeApp) {
         if (fragment != null) {
@@ -111,38 +77,5 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return false;
-    }
-
-
-    //TODO mettre cela dans le "Scanner" class
-    // Gestionnaire d'événements pour le clic sur le bouton "De ma bibliothèque"
-    private void onFromLibraryButtonClick(View view) {
-        // Créer un Intent pour ouvrir la galerie de photos
-        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(galleryIntent, REQUEST_PICK_IMAGE);
-    }
-
-    //TODO mettre cela dans le "Scanner" class
-    // Méthode pour gérer le résultat de la sélection d'image depuis la bibliothèque de photos
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_PICK_IMAGE && resultCode == Activity.RESULT_OK && data != null) {
-            // L'utilisateur a sélectionné une image avec succès
-            Uri selectedImageUri = data.getData();
-            // Faites quelque chose avec l'URI de l'image sélectionnée, par exemple affichez-la dans une ImageView
-            ImageView imageView = findViewById(R.id.click_image);
-            imageView.setImageURI(selectedImageUri);
-
-            // Appel de la méthode pour scanner le code QR à partir de l'image sélectionnée
-            scanQRCodeFromImage(selectedImageUri);
-        }
-    }
-
-    //TODO mettre cela dans le "Scanner" class
-    // Méthode pour scanner un code QR à partir d'une image
-    private void scanQRCodeFromImage(Uri imageUri) {
-        // Ajoutez le code nécessaire pour scanner le code QR à partir de l'image ici
-        // Vous pouvez utiliser le code fourni dans les réponses précédentes pour implémenter cette fonctionnalité
     }
 }
